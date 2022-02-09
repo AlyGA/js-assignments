e9938097d5de1a2b38733286faa9dc843cef5fd9
Aly Anderson <aly779372@gmail.com>
2022-02-09 13:36:40 -0800

    Create solution for fourth lecture

---
+++ fundamentals-one/basic-operations/script.js
@@ -0,0 +1,16 @@
+let country = 'US';
+let continent = 'NA';
+const population = 10000;
+let isIsland = false;
+const finlandPopulation = 6000000
+let language = 'English';
+
+halfPopulation = population / 2; //5000
+halfPopulation ++; // halfPopulation = halfPopulation + 1 // 5001
+console.log(halfPopulation);
+console.log(finlandPopulation < population);
+console.log(33000000 > population);
+
+description = country + ' is in ' + continent + ', and its ' + population + ' people speak ' + language;
+
+console.log(description);
