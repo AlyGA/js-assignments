bdcd3b617255fb00778c75e7764311fe7c1d3cc1
Aly Anderson <aly779372@gmail.com>
2022-02-09 10:33:14 -0800

    Move folders into one big folder

---
+++ fundamentals-one/data-types/script.js
@@ -0,0 +1,12 @@
+let country = 'US';
+let continent = 'NA';
+let population = '10,000';
+let isIsland = false;
+let language;
+
+
+console.log(typeof country);
+console.log(typeof continent);
+console.log(typeof population);
+console.log(typeof isIsland);
+console.log(typeof language);
