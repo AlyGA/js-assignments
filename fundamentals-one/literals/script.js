28266fcf3f0a4b9bfd3b7be8e4101022054c2164
Aly Anderson <aly779372@gmail.com>
2022-02-10 16:21:24 -0800

    Add my solution to the fifth assignment

---
+++ fundamentals-one/literals/script.js
@@ -0,0 +1,13 @@
+let country = 'US';
+let continent = 'NA';
+const population = 10000;
+let isIsland = false;
+const finlandPopulation = 6000000
+let language = 'English';
+
+halfPopulation = population / 2; //5000
+halfPopulation ++; // halfPopulation = halfPopulation + 1 // 5001
+
+description = `The ${country} is in ${continent}, and its ${population} people speak ${language}.`
+
+console.log(description);
