4da88fcfe32b0c43017c82ab681410e4be159816
Aly Anderson <aly779372@gmail.com>
2022-02-12 13:07:37 -0800

    Added solution and notes for sevethn lecture

---
+++ fundamentals-one/type-coercion/script.js
@@ -0,0 +1,6 @@
+console.log('9' - '5'); // 4
+console.log('19' - '13' + '17'); // 617
+console.log('19' + '13' + 17); // 191317
+console.log('237' < 15); // False
+console.log(5 + 6 + '4' + 9 - 4 - 2); // 1143
+// The order for this one was (5+6) + '4', which is '114' then that string with 9 - 4 - 2 which is 3. So, '1143'.
