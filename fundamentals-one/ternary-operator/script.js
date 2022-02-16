629c0a905391fb339692129afa7b42db660e3668
Aly Anderson <aly779372@gmail.com>
2022-02-16 12:23:05 -0800

    Update solution for final lecture

---
+++ fundamentals-one/ternary-operator/script.js
@@ -0,0 +1,10 @@
+const country = 'America';
+const continent = 'NA';
+const population = 103000000;
+const isIsland = false;
+const language = 'English';
+
+
+let result = population >= 33000000 ? `${country}'s population is above average!`: `${country}'s population is below average!`
+
+console.log(result)
