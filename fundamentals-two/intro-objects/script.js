f87224eb4f77b55384cee30770a3b0f0a21c41e0
Aly Anderson <aly779372@gmail.com>
2022-02-25 10:17:19 -0800

    Create object for details of America

---
+++ fundamentals-two/intro-objects/script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+// Create an Object
+const myCountry = {
+    country: 'America',
+    capital: 'Washington',
+    language: 'English',
+    population: 579000000,
+    neighbors: ['Canada', 'Brazil', 'Greenland']
+}
